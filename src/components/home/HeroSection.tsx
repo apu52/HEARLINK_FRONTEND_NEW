19943158a98ca86b4c8e3516ee5028c7e6cc5a56
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2025-05-02 22:59:42 +0530

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -69,7 +69,7 @@ const HeroSection = () => {
           <div className="relative">
             <div className="relative z-10 rounded-xl overflow-hidden shadow-xl animate-fade-in">
               <img
-                src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+                src="images\istockphoto-1280356530-612x612.jpg"
                 alt="Students learning with HearLink"
                 className="w-full h-full object-cover"
               />
