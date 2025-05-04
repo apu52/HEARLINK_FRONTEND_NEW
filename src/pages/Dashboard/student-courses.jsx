e75c6c4a9935c3093fc4ea9244e59f54c70c82f0
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2025-05-04 17:42:26 +0530

    Update student-courses.jsx

--- src/pages/Dashboard/student-courses.jsx
+++ src/pages/Dashboard/student-courses.jsx
@@ -175,7 +175,7 @@ const MyCourses = () => {
             </li>
             <li>
               <Link
-                to="/speech-to-text"
+                to="https://monumental-starlight-979bdd.netlify.app/"
                 className="flex items-center p-2 rounded-lg hover:bg-hearlink-50 text-gray-600"
               >
                 <Headphones className={`h-5 w-5 ${collapsed ? "mx-auto" : "mr-3"}`} />
